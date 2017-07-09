b83c1826b9c7dffed3aa734240d668da83d703bb
khoatran <khoatran.kt1991@gmail.com>
2017-07-09 23:59:10 +0700

    Scrolling Features

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.static('public'));
 app.set('views', 'view');
 app.set('view engine', 'ejs');
-app.get('/', (req, res)=>res.render('index'));
+app.get('/', (req, res)=>res.render('template1'));
 app.listen(3000, function(e) {
     console.log("Server is starting in PORT 3000");
 });
